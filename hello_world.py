92a3eb68ec8962c65b90cc6f28f4c254e107edd6
Kody Simpson <kodysimpson0616@gmail.com>
2022-01-02 00:17:37 -0600

    init

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# Your first Python Program.
+# it is tradition in programming for your first program to be one that outputs "Hello World"
+
+print("Hello, World!")  # outputs the thing in quotes
+
+print("My name is Kody Simpson and I think Python is pretty cool.")
+
+print(23 * 3)
